6c35be781ea3a683fc36301bf92722788851958e
migzpogi <migz.zgim@gmail.com>
2022-01-04 13:26:57 +0800

    Finishes day 03

--- day03.py
+++ day03.py
@@ -8,10 +8,20 @@ class DigitCount:
         self.zero = 0
 
     def most_common(self):
-        if self.one > self.zero:
+        if self.one == self.zero:
+            return '1'
+        elif self.one > self.zero:
             return '1'
         else:
             return '0'
+
+    def least_common(self):
+        if self.one == self.zero:
+            return '0'
+        elif self.one > self.zero:
+            return '0'
+        else:
+            return '1'
 
 
 class Report:
@@ -48,33 +58,111 @@ def input_file_to_list(file_path):
     return file_contents
 
 
-r = Report()
+# r = Report()
+#
+# for i in range(1, 13):
+#     r.digits.append(DigitCount(i))
+#
+# bits = input_file_to_list("day03_input.txt")
+# for i, bit in enumerate(bits):
+#     for j, digit in enumerate(bit):
+#         if int(digit) == 0:
+#             r.increment_zero(j+1)
+#         else:
+#             r.increment_one(j+1)
+#
+# gamma = ""
+# epsilon = ""
+#
+# for x in r.digits:
+#     gamma += x.most_common()
+#
+# for x in gamma:
+#     if x == '1':
+#         epsilon += '0'
+#     else:
+#         epsilon += '1'
+#
+# print(gamma)
+# g = (int(gamma, 2))
+# print(epsilon)
+# e = (int(epsilon, 2))
+# print(g*e)
 
-for i in range(1, 13):
-    r.digits.append(DigitCount(i))
+
 
 bits = input_file_to_list("day03_input.txt")
-for i, bit in enumerate(bits):
-    for j, digit in enumerate(bit):
-        if int(digit) == 0:
-            r.increment_zero(j+1)
+idx = 0
+remaining = len(bits)
+while remaining > 1:
+    x_count = 0
+    r = Report()
+    for i in range(1, 13):
+        r.digits.append(DigitCount(i))
+
+    for bit in bits:
+        if bit == 'x':
+            continue
+
+        if bit[idx] == '0':
+            r.increment_zero(idx+1)
         else:
-            r.increment_one(j+1)
+            r.increment_one(idx+1)
 
-gamma = ""
-epsilon = ""
+    most_common = r.get(idx+1).most_common()
 
-for x in r.digits:
-    gamma += x.most_common()
+    for i, j in enumerate(bits):
+        if j == 'x':
+            continue
 
-for x in gamma:
-    if x == '1':
-        epsilon += '0'
+        if j[idx] != most_common:
+            bits[i] = 'x'
+            x_count += 1
+
+    remaining = remaining - x_count
+    idx += 1
+
+for i in bits:
+    if i == 'x':
+        continue
     else:
-        epsilon += '1'
+        print(i)
+        print(int(i, 2))
 
-print(gamma)
-g = (int(gamma, 2))
-print(epsilon)
-e = (int(epsilon, 2))
-print(g*e)
+bits = input_file_to_list("day03_input.txt")
+idx = 0
+remaining = len(bits)
+while remaining > 1:
+    x_count = 0
+    r = Report()
+    for i in range(1, 13):
+        r.digits.append(DigitCount(i))
+
+    for bit in bits:
+        if bit == 'x':
+            continue
+
+        if bit[idx] == '0':
+            r.increment_zero(idx+1)
+        else:
+            r.increment_one(idx+1)
+
+    most_common = r.get(idx+1).least_common()
+
+    for i, j in enumerate(bits):
+        if j == 'x':
+            continue
+
+        if j[idx] != most_common:
+            bits[i] = 'x'
+            x_count += 1
+
+    remaining = remaining - x_count
+    idx += 1
+
+for i in bits:
+    if i == 'x':
+        continue
+    else:
+        print(i)
+        print(int(i, 2))
