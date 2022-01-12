b0cb02e782a4077365e313c0f93136faf12afbde
migzpogi <migz.zgim@gmail.com>
2022-01-12 10:18:26 +0800

    Starts day 9

---
+++ day09.py
@@ -0,0 +1,17 @@
+def input_file_to_list(file_path):
+    """
+    Opens the input file for the problem and store line by line contents in a list
+    :param file_path:
+    :return:
+    """
+    file_contents = []
+    with open(file_path, "r+") as f:
+        for line in f.readlines():
+            file_contents.append(line.strip())
+
+    return file_contents
+
+
+points = input_file_to_list("day09_input.txt")
+for p in points:
+    print(len(p))
